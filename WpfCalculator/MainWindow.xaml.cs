b017806259bb2d1397bec9c4ef075a288708daa2
fingerissue <fingerissue@gmail.com>
2025-09-17 23:56:54 +0900

    Feat: Add keyboard input support

--- WpfCalculator/MainWindow.xaml.cs
+++ WpfCalculator/MainWindow.xaml.cs
@@ -126,5 +126,78 @@ namespace WpfCalculator
                 ResultTextBlock.Text = currentValue.ToString();
             }
         }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.D0:
+                case Key.NumPad0:
+                    NumberButton_Click(Button0, null);
+                    break;
+                case Key.D1:
+                case Key.NumPad1:
+                    NumberButton_Click(Button1, null);
+                    break;
+                case Key.D2:
+                case Key.NumPad2:
+                    NumberButton_Click(Button2, null);
+                    break;
+                case Key.D3:
+                case Key.NumPad3:
+                    NumberButton_Click(Button3, null);
+                    break;
+                case Key.D4:
+                case Key.NumPad4:
+                    NumberButton_Click(Button4, null);
+                    break;
+                case Key.D5:
+                case Key.NumPad5:
+                    NumberButton_Click(Button5, null);
+                    break;
+                case Key.D6:
+                case Key.NumPad6:
+                    NumberButton_Click(Button6, null);
+                    break;
+                case Key.D7:
+                case Key.NumPad7:
+                    NumberButton_Click(Button7, null);
+                    break;
+                case Key.D8:
+                case Key.NumPad8:
+                    NumberButton_Click(Button8, null);
+                    break;
+                case Key.D9:
+                case Key.NumPad9:
+                    NumberButton_Click(Button9, null);
+                    break;
+                case Key.OemPeriod:
+                case Key.Decimal:
+                    NumberButton_Click(ButtonDemical, null);
+                    break;
+
+                case Key.Add:
+                    OperatorButton_Click(ButtonAdd, null);
+                    break;
+                case Key.Subtract:
+                    OperatorButton_Click(ButtonSubtract, null);
+                    break;
+                case Key.Multiply:
+                    OperatorButton_Click(ButtonMultiply, null);
+                    break;
+                case Key.Divide:
+                    OperatorButton_Click(ButtonDivide, null);
+                    break;
+
+                case Key.Enter:
+                    EqualsButton_Click(ButtonEquals, null);
+                    break;
+
+                case Key.Escape:
+                case Key.Back:
+                    ClearButton_Click(ButtonClear, null);
+                    break;
+            }
+        }
     }
 }
